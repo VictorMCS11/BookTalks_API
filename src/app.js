bb94089c6ab0d8df41166a012dd894d5ac8b27a5
Victor Manuel <VictorMCS11@gmail.com>
2025-02-20 00:57:32 +0100

    cors añadidos ++

--- src/app.js
+++ src/app.js
@@ -23,20 +23,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'images/bookcovers')))
 app.use(cors({
-    origin: function (origin, callback) {
-        // Lista de orígenes permitidos
-        const allowedOrigins = [
-            'http://185.166.39.53', // IP de tu frontend en Hostinger
-            'http://localhost:5174', // Si quieres permitir tu frontend en localhost
-            'http://91.242.150.149'
-        ];
-
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) { // !origin permite acceso desde herramientas como Postman
-            callback(null, true); // Permite el origen
-        } else {
-            callback(new Error('No permitido por CORS'), false); // Rechaza la solicitud
-        }
-    },
+    origin: '*',  // Permite todos los orígenes (no recomendado en producción),
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
     credentials: true, // Si necesitas enviar cookies o encabezados de autenticación
 }));
